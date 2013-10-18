91af69efe7d14acaf79f88da0a23c7d2124bc5d4
Jesse Shawl <jesseshawl@gmail.com>
2013-10-18 00:34:29 +0000

    initial commit

---
+++ sitefinder/index.php
@@ -0,0 +1,148 @@
+<?
+include("/home/agriserv/public_html/randomlink/dodosrangen.php");
+?>
+<html>
+<head>
+<title> Cattle Today's SiteFinder </title>
+<meta NAME="Description" CONTENT="Cattle Today's Site Finder Links Page. Your Guide to the Websites of the Cattle Today Network.">
+<meta NAME="keywords" CONTENT="Links,horses,cattle,livestock">
+<meta NAME="Rating" CONTENT="General">
+<meta NAME="Distribution" CONTENT="Global">
+<meta NAME="Revisit-after" CONTENT="7 days">
+<meta NAME="copyright" CONTENT="Copyright 1998-2007 Cattle Today, Inc.  All rights reserved">
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+
+<body>
+<div align="center"> 
+  <table width="600" border="0" align="center">
+    <tr>
+      <td width="537" valign="middle">
+<div align="center"> 
+          <h1>&nbsp;</h1>
+          
+        <table width="598" border="0" cellpadding="6">
+          <tr> 
+            <td align="center"> <h1 align="center"><img src="/sitefinder/images/sitefinder.gif" alt="Cattle Today's SiteFinder" width="269" height="98"><br>
+                <font face="Arial Black, Arial, sans-serif">Your Guide to the 
+                Websites of the Cattle Today Network.</font></h1>
+              <p align="center">&nbsp;</p></td>
+          </tr>
+          <tr> 
+            <td colspan="2" align="center"> <div align="center"><a href="http://ranchweather.com"><img src="/sitefinder/images/ct_weather.gif" alt="Agricural weather agriculture" width="140" height="50" border="1"></a><br>
+                <a href="http://ranchweather.com"><font size="2" face="Arial, Helvetica, sans-serif">Weather 
+                for Agriculture</font></a><font size="2" face="Arial, Helvetica, sans-serif"><br>
+                Agricultural weather including 10 day forecast, radar, drought 
+                index and much more.</font></div></td>
+          </tr>
+
+          <tr> 
+            <td colspan="2" align="center"> <div align="center"><font size="2" face="Arial, Helvetica, sans-serif"><a href="http://cattle-today.com"><img src="/sitefinder/images/c-t_com.gif" alt="cattle-today.com" width="140" height="50" border="1"></a><br>
+                <a href="http://cattle-today.com">Beef Cattle Breeds</a><br>
+                A listing of the major breeds of beef cattle including pictures 
+                and descriptions.</font></div></td>
+          </tr>
+          <tr> 
+            <td colspan="2" align="center"> <div align="center"><font size="2" face="Arial, Helvetica, sans-serif"><a href="http://cattletoday.info"><img src="/sitefinder/images/ct_info.gif" alt="cattletoday.info" width="140" height="50" border="1"></a><br>
+                <a href="http://cattletoday.info">Cattle Diseases</a><br>
+                Major cattle diseases, their causes, symptons and treatment.</font></div></td>
+          </tr>
+          <tr> 
+            <td colspan="2" align="center"> <div align="center"><font size="2" face="Arial, Helvetica, sans-serif"><a href="http://cattletoday.us"><img src="/sitefinder/images/ct_us.gif" alt="Cattle Today" width="140" height="50" border="1"></a><br>
+                <a href="http://cattletoday.us">Cattle Today Site Finde</a>r<br>
+                Your Guide to the Websites of the Cattle Today Network.</font> 
+                <font size="2" face="Arial, Helvetica, sans-serif"><br>
+                </font></div></td>
+          </tr>
+          <tr> 
+            <td colspan="2" align="center"> <div align="center"><font size="2" face="Arial, Helvetica, sans-serif"><a href="http://cattletoday.net"><img src="/sitefinder/images/ct_net.gif" alt="hay for sale" width="140" height="50" border="1"></a><br>
+                <a href="http://cattletoday.net">Hay for Sale</a><br>
+                The Hay &amp; Grain Network, your site to buy and sell hay and 
+                grain.</font></div></td>
+          </tr>
+
+          <tr> 
+            <td colspan="2" align="center"> <div align="center"> 
+                <p><a href="http://ranchlinks.com"><img src="/sitefinder/images/ct_links.gif" alt="ranch cattle links" width="140" height="50" border="1"></a><br>
+                  <a href="http://ranchlinks.com"><font size="2" face="Arial, Helvetica, sans-serif">Ranch 
+                  &amp; Cattle Links</font></a><font size="2" face="Arial, Helvetica, sans-serif"><br>
+                  Thousands of cattle, ranch and agricultural links. Add yours 
+                  today! </font></p>
+              </div></td>
+          </tr>
+          <tr> 
+            <td colspan="2" align="center"> <div align="center"><a href="http://horsepages.com"><img src="/sitefinder/images/ct_pages.gif" alt="horse pages" width="140" height="50" border="1"></a><br>
+                <a href="http://horsepages.com"><font size="2" face="Arial, Helvetica, sans-serif">Horse 
+                Pages</font></a><font size="2" face="Arial, Helvetica, sans-serif"><br>
+                Horses Links, Greeting Cards, Free Photo Classifieds and a Forum. 
+                </font></div></td>
+          </tr>
+          <tr> 
+            <td colspan="2" align="center"> <div align="center"><a href="http://horselover.net"><font size="2" face="Arial, Helvetica, sans-serif"><img src="/sitefinder/images/ct_lover.gif" alt="horse lover" width="140" height="50" border="1"></font></a><font size="2" face="Arial, Helvetica, sans-serif"><br>
+                <a href="http://horselover.net">Horse Lover</a><br>
+                The place for horse people with free classified ads, links, forums,stories 
+                and greeting cards</font>. </div></td>
+          </tr>
+          <tr> 
+            <td colspan="2" align="center"> <div align="center"><a href="http://catfishnews.com"><img src="/sitefinder/images/ct_fish.gif" alt="catfish news" width="140" height="50" border="1"></a><br>
+                <a href="http://catfishnews.com"><font size="2" face="Arial, Helvetica, sans-serif">Catfish 
+                News and Information</font></a><font size="2" face="Arial, Helvetica, sans-serif"><br>
+                USDA market news, classified ads, links and information for the 
+                farm raised catfish industry.</font><br>
+              </div></td>
+          </tr>
+          <tr> 
+            <td colspan="2" align="center"><p><a href="http://ringlinks.net"><img src="/sitefinder/images/ct_ring.gif" alt="RingLinks and WebRings" width="140" height="50" border="1"></a><br>
+                <font size="2" face="Arial, Helvetica, sans-serif"><a href="http://ringlinks.net">WebRings 
+                and RingLinks</a></font><br>
+                <font size="2" face="Arial, Helvetica, sans-serif">Our ag related 
+                WebRings and RingLinks you can join.</font></p></td>
+          </tr>
+          <tr>
+            <td colspan="2" align="center"><p><a href="http://agdomainnames.com/"><img src="../sitefinder/images/ct_agnames.gif" width="140" height="50" border="1"></a>
+                <br><a href="http://agdomainnames.com/"><font size="2" face="Arial, Helvetica, sans-serif">Agricultural 
+                  Domain Names for Sale</font></a><font size="2" face="Arial, Helvetica, sans-serif"><br>
+                  Dozens of unique, easy to remember domain names for sale.</font></p><br>
+                </td>
+          </tr>
+          <tr> 
+            <td height="49" colspan="2" align="center" valign="bottom"><table border="4" width="600" bgcolor="#F1EBE7" bordercolor="#008000" bordercolorlight="#663300" cellpadding="6" background="/backgrounds/bg08yellow.gif" bordercolordark="#663300">
+                <tr> 
+                  <td><center>
+                      <h1 align="center"><font face="Arial" size="4"><br>
+                        </font><strong><font color="#663300" face="Arial" size="7">FAVORITE WEB 
+                        SITES</font></strong></h1>
+                      <p align="center"><font face="Verdana" size="4">These are 
+                        some of the Web Sites we have created and maintain for 
+                        our customers.&nbsp; If you would like more information 
+                        about us creating a Web Site just for <u>you</u>,&nbsp;<br>
+                        </font><font face="Verdana" size="4"><a href="http://cattletoday.com/webdesign.htm">CLICK 
+                        HERE</a>.</font></p>
+                      <p align="center"><font face="Verdana" size="3">Just click 
+                        on the name to visit site!</font></p>
+                    </center></td>
+                </tr>
+                <tr> 
+                  <td><div align="center"><font face="Verdana" size="3"><b>
+                      <?dodosrandgen();?>
+                      </b> </font></center></div></td>
+                </tr>
+              </table></table></td>
+    </tr>
+            <tr>
+              <td height="49" colspan="2" align="center" valign="bottom"><p><font size="1" face="Arial, Helvetica, sans-serif">Copyright &#169; 2001-<?php
+  echo date("Y");
+  ?><br>
+                All rights reserved.
+              </font><font size="1" face="Arial, Helvetica, sans-serif"><br>
+              <a href="http://cattletoday.com">Cattle Today, Inc.</a></font></p>
+                </td>
+            </tr>
+  </table>
+          
+</div></td>
+    </tr>
+  </table>
+</body>
+</html>
